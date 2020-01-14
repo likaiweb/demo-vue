e9ae5828a0eebfba111b59064b24e12fd2d8efd0
likaiweb <869546886@qq.com>
2020-01-14 17:12:06 +0800

    output

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,12 @@
  * @Date: 2019-12-25 16:25:46
  * @Author: 李凯
  * @LastEditors  : 李凯
- * @LastEditTime : 2019-12-26 15:30:32
+ * @LastEditTime : 2020-01-14 15:38:42
  * @Description: 配置文件
  * @FilePath: /vue-test/vue.config.js
  */
-const path=require('path');
+const path = require('path');
+const time = new Date().getTime();
 module.exports = {
     outputDir: '/' + process.env.outputDir + '/',
     lintOnSave: false,  // 禁止报错
@@ -18,5 +19,11 @@ module.exports = {
                 path.resolve(__dirname,'./src/style/theme.less')
             ]
         }
-    }
+    },
+    configureWebpack: { // webpack 配置
+        output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+            filename: `js/[name].${time}.js`,
+            chunkFilename: `js/[name].${time}.js`
+        },
+    },
 }
